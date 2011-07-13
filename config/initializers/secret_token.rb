e01d41e87dd46d2fb8dee00a1a0390e108afa396
Kenneth Falck <kennu@iki.fi>
2011-07-13 15:22:08 +0300

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teletextfeed::Application.config.secret_token = '52afdc3b99035fd388aaa16709213bdb131aedf5554561f0651d1579a7d0f562fa1aae760213fb4f26c72420c072c7a1812add38adc4542c28c2fc80bf8d9b4e'
